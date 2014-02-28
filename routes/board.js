235cbb63cc12f341d613bc0ecb6de8b8e072530c
larahy <larahy@gmail.com>
2014-02-28 16:24:04 +0000

    scoreboard on game page

--- routes/board.js
+++ routes/board.js
@@ -1,6 +1,19 @@
 
-module.exports = {
-  index: function(req, res, next) {
-    res.render('boards/index');
-  }
+// module.exports = {
+//   index: function(req, res, next) {
+//     res.render('boards/index');
+//   }
+// };
+
+var Scores = require('./../scores');
+
+module.exports.index = function(req, res, next) {
+  var scores = new Scores();
+  scores.findAll(function(err, scores) {
+    if (err) return next(err);
+
+    res.render('boards/index', { scores: scores || [] });
+  });
 };
+
+
